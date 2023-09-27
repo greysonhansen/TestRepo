a0273d10344660e9f96bc89cf958b82d2e5db0b4
Greyson <greysonhansen21@augustana.edu>
2023-09-27 00:09:47 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello bird world!");
+
+        Chicken chicken = new Chicken();
+        chicken.dance();
     }
 }
